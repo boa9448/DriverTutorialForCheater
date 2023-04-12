50326d712d2f1651489ea12c952e6ab69cf5836c
boa9448 <boa3465@gmail.com>
2023-04-12 19:33:22 +0900

    잘못된 UNREFERENCED_PARAMETER 제거

--- src/SSDTIndex/driver.cpp
+++ src/SSDTIndex/driver.cpp
@@ -12,7 +12,6 @@ void DriverUnload(PDRIVER_OBJECT driver_object)
 
 EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path)
 {
-    UNREFERENCED_PARAMETER(driver_object);
     UNREFERENCED_PARAMETER(registry_path);
     driver_object->DriverUnload = DriverUnload;
 
