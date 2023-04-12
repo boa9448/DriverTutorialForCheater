ec8e226496c11c93d2d36aa64ddbbf2ccbc5baf4
boa9448 <boa3465@gmail.com>
2023-04-12 19:40:17 +0900

    사소한 수정

--- src/SSDTIndex/ssdt_index.hpp
+++ src/SSDTIndex/ssdt_index.hpp
@@ -5,7 +5,7 @@
 
 namespace ssdt_index
 {
-    constexpr ULONG PE_ERROR = (ULONG) - 1;
+    constexpr ULONG PE_ERROR = (ULONG) -1;
 
     int get_export_ssdt_index(LPCSTR func_name);
     int get_export_ssdt_shadow_index(LPCSTR func_name);
